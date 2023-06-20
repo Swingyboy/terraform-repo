ee40c7bd00a4cf16aa9204d9adad32bd4761a4b8
Swingyboy <kedonosec@gmail.com>
2023-06-20 17:21:54 +0000

    update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,18 @@ terraform {
   }
 }
 
+module "flux_bootstrap" {
+  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=gke_auth"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  private_key       = module.tls_private_key.private_key_pem
+  config_host       = module.gke_cluster.config_host
+  config_token       = module.gke_cluster.config_token
+  config_ca         = module.gke_cluster.config_ca
+  github_token      = var.GITHUB_TOKEN
+}
+
 module "gke_cluster" {
-  source         = "github.com/Swingyboy/tf-google-gke-cluster"
+  source         = "github.com/den-vasyliev/tf-google-gke-cluster?ref=gke_auth"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = var.GKE_NUM_NODES
@@ -21,15 +31,28 @@ module "github_repository" {
   public_key_openssh_title = "flux"
 }
 
-module "flux_bootstrap" {
-  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap"
-  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
-  private_key       = module.tls_private_key.private_key_pem
-  config_path       = module.gke_cluster.kubeconfig
-  github_token      = var.GITHUB_TOKEN
-}
-
 module "tls_private_key" {
   source    = "github.com/den-vasyliev/tf-hashicorp-tls-keys"
   algorithm = "RSA"
+}
+
+module "gke-workload-identity" {
+  source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  use_existing_k8s_sa = true
+  name                = "kustomize-controller"
+  namespace           = "flux-system"
+  project_id          = var.GOOGLE_PROJECT
+  cluster_name        = "main"
+  location            = var.GOOGLE_REGION
+  annotate_k8s_sa     = true
+  roles               = ["roles/cloudkms.cryptoKeyEncrypterDecrypter"]
+}
+
+module "kms" {
+  source          = "github.com/den-vasyliev/terraform-google-kms"
+  project_id      = var.GOOGLE_PROJECT
+  keyring         = "sops-flux1"
+  location        = "global"
+  keys            = ["sops-key-flux"]
+  prevent_destroy = false
 }
